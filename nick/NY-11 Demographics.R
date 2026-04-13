bfaf683488bc3e06a6776532ac316639cb00ec76
Nick Ramoso <ramoson@spu.edu>
2026-04-13 14:41:53 -0700

    Demographics minority

--- nick/NY-11 Demographics.R
+++ nick/NY-11 Demographics.R
@@ -6,7 +6,7 @@ library(ggredist)
 library(redist)
 library(tidycensus)
 library(tigris)
-
+library(sf)
 
 ### New York and Missouri Maps
 map_ny <- alarm_50state_map('NY')
@@ -49,5 +49,52 @@ ggplot() +
 
 
 census_api_key("d84f6074a7e95b068d1dbc2a2daeb05bf6ab224b", install = TRUE)
+
+get_acs(
+  geography = "state",
+  variables = "B01003_001",
+  year = 2022
+)
+
+vars <- c(
+  total = "B01003_001",
+  white = "B02001_002",
+  black = "B02001_003",
+  hispanic = "B03003_003"
+)
+
+acs_data <- get_acs(
+  geography = "tract",
+  variables = vars,
+  state = "NY",
+  year = 2022,
+  geometry = TRUE
+)
+
+ny11_demo <- st_intersection(acs_data, ny11_current)
+
+ny11_demo_wide <- ny11_demo |>
+  select(GEOID, variable, estimate, geometry) |>
+  pivot_wider(names_from = variable, values_from = estimate) |>
+  mutate(
+    minority_pct = 1 - (white / total)
+  )
+
+ggplot(ny11_demo_wide) +
+  geom_sf(aes(fill = minority_pct), color = NA) +
+  scale_fill_viridis_c(labels = scales::percent) +
+  labs(
+    title = "Minority Population Share in NY District 11",
+    fill = "% Minority"
+  )
+
+
+
+
+
+
+
+
+
 
 
